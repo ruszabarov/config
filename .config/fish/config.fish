1397176639c8a9281b20cb3aac3221b578abee1a
Your name <ruszabarov@gmail.com>
2024-06-16 00:14:31 -0400

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+alias ls="eza --all --color=always --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+
+eval "$(zoxide init fish)"
+
+alias cd="z"
+alias vim="nvim"
