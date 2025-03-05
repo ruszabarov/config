faa1c7544cf5155c7468bca9b523a99cd0d72617
Ruslan Zabarov <ruszabarov@gmail.com>
2025-03-04 19:11:17 -0500

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,9 @@ alias vim="nvim"
 set -Ux PATH $PATH (go env GOPATH)/bin
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /Users/ruszabarov/.ghcup/bin # ghcup-env
+
+set -x GPG_TTY (tty)
+
+set -x JAVA_HOME (/usr/libexec/java_home)
+set -x ANT_HOME (brew --prefix ant)/libexec
+set -x PATH $JAVA_HOME/bin $ANT_HOME/bin $PATH
